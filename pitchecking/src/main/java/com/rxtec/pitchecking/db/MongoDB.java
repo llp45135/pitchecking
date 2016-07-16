ab75ec38b61fb9a90120fe59a3fe5abce693e987
llp_l <llp_l@DESKTOP-HUIINAA>
2016-07-16 14:22:38 +0800

    0.6.3

--- pitchecking/src/main/java/com/rxtec/pitchecking/db/MongoDB.java
+++ pitchecking/src/main/java/com/rxtec/pitchecking/db/MongoDB.java
@@ -1,11 +1,16 @@
 package com.rxtec.pitchecking.db;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.DeleteResult;
@@ -15,37 +20,37 @@ import com.rxtec.pitchecking.picheckingservice.PITVerifyData;
 public class MongoDB {
 	private MongoClient client;
 	private MongoDatabase db;
-	MongoCollection<DBObject> passedCollections ;
-	MongoCollection<DBObject> failedCollections ;
+	MongoCollection<DBObject> passedCollections;
+	MongoCollection<DBObject> failedCollections;
 
 	SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
 	private static MongoDB _instance = new MongoDB();
 
-	private MongoDB(){
-		client = new MongoClient(Config.getInstance().getMongoDBAddress(),
-				Config.getInstance().getMongoDBPort());
+	private MongoDB() {
+		String uri = "mongodb://pitcheck_writer:PitcheckWriter61336956@localhost:27017/?authSource=pitcheck";
+	    client = new MongoClient(new MongoClientURI(uri));
 		db = client.getDatabase(Config.MongoDBName);
 		passedCollections = db.getCollection(Config.PassedMongoCollectionName, DBObject.class);
 		failedCollections = db.getCollection(Config.PassedMongoCollectionName, DBObject.class);
 	}
-	
+
 	public static synchronized MongoDB getInstance() {
 		if (_instance == null)
 			_instance = new MongoDB();
 		return _instance;
 	}
-	
-	public void save(PITVerifyData data,boolean isPassed){
+
+	public void save(PITVerifyData data, boolean isPassed) {
 		DBObject rec = new BasicDBObject();
 		rec.put("id_no", data.getIdNo());
-		rec.put("name",data.getPersonName());
+		rec.put("name", data.getPersonName());
 		rec.put("age", data.getAge());
 		rec.put("frame_image", data.getFaceImg());
 		rec.put("id_image", data.getIdCardImg());
 		rec.put("face_image", data.getFaceImg());
 		rec.put("verify_result", data.getVerifyResult());
 		DBObject ticket = new BasicDBObject();
-		rec.put("ticket",ticket);
+		rec.put("ticket", ticket);
 		ticket.put("train_no", data.getTicket().getTrainCode());
 		ticket.put("train_date", data.getTicket().getTrainDate());
 		ticket.put("from_station", data.getTicket().getFromStationCode());
@@ -55,36 +60,28 @@ public class MongoDB {
 
 		rec.put("in_date", sf.format(new Date()));
 		rec.put("gate_no", data.getTicket().getInGateNo());
-		
-		if(isPassed) passedCollections.insertOne(rec);
-		else failedCollections.insertOne(rec);
-		
-		
+
+		if (isPassed)
+			passedCollections.insertOne(rec);
+		else
+			failedCollections.insertOne(rec);
+
 	}
-	
-	
-	public void clearExpirationData(){
+
+	public void clearExpirationData() {
 		int days = Config.getInstance().getFaceLogRemainDays();
 		SimpleDateFormat sFormat = new SimpleDateFormat("yyyyMMdd");
 
 		Date today = new Date();
 		String dn = sFormat.format(new Date(today.getTime() - days * 24 * 60 * 60 * 1000));
-		BasicDBObject query = new BasicDBObject();  
+		BasicDBObject query = new BasicDBObject();
 		query.put("in_date", new BasicDBObject("$lte", dn));
 		DeleteResult result = passedCollections.deleteMany(query);
-		System.out.println("Delete passedCollections "+result.getDeletedCount() + " docs");
-		
+		System.out.println("Delete passedCollections " + result.getDeletedCount() + " docs");
+
 		result = failedCollections.deleteMany(query);
-		System.out.println("Delete failedCollections "+result.getDeletedCount() + " docs");
+		System.out.println("Delete failedCollections " + result.getDeletedCount() + " docs");
 
-		
 	}
-
-
-
-
-
-
-
 
 }
